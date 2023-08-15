2a8e9a506e623b60ff92aaec00cb5a44f42905c8
MalinaEva <teqi@yandex.ru>
2023-08-16 03:05:32 +0700

    refactor: linter fix

--- validations/cardValidations.js
+++ validations/cardValidations.js
@@ -2,8 +2,7 @@ const Joi = require('joi');
 
 exports.createCardValidationSchema = Joi.object({
   name: Joi.string().min(2).max(30).required(),
-  link: Joi.string().uri().required(),
-  owner: Joi.string().pattern(/^[0-9a-fA-F]{24}$/).required(),
+  link: Joi.string().uri().pattern(/^https?:\/\/(www\.)?[\w\-._~:/?#[\]@!$&'()*+,;=]+#?$/).required(),
   likes: Joi.array().default([]),
   createdAt: Joi.date().default(Date.now),
 }).options({ stripUnknown: true });
